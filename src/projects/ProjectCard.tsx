9a35d3a5238121f566b6a58f8844f738555042f1
asfreeasfly <simonbergman516@gmail.com>
2023-09-25 00:48:51 +0200

    implement Lab 8: More Reusable Component

---
+++ src/projects/ProjectCard.tsx
@@ -0,0 +1,27 @@
+import { Project } from "./Project";
+
+interface ProjectCardProps {
+    project: Project;
+}
+
+function formatDescription(description: string): string {
+    return description.substring(0, 60) + "...";
+}
+
+function ProjectCard(props: ProjectCardProps) {
+    const { project } = props;
+    return (
+        <div className="card">
+            <img src={project.imageUrl} alt={project.name}></img>
+            <section className="section dark">
+                <h5 className="strong">
+                    <strong>{project.name}</strong>
+                </h5>
+                <p>{formatDescription(project.description)}</p>
+                <p>Budget: {project.budget}</p>
+            </section>
+        </div>
+    );
+}
+
+export default ProjectCard;
--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Project } from "./Project";
-
+import ProjectCard from "./ProjectCard";
 interface ProjectListProps {
     projects: Project[];
 }
@@ -9,25 +9,9 @@ function ProjectList({ projects }: ProjectListProps) {
     return (
         <div className="row">
             {projects.map((project) => (
-                <div key={project.id} className="cols-sm">
-                    <div className="card">
-                        <img src={project.imageUrl} alt={project.name}></img>
-                        <article className="section dark">
-                            <h5 className="strong">
-                                {project.name}
-                            </h5>
-                            <p>{project.description}</p>
-                            <p>Budget: {project.budget.toString()}</p>
-                        </article>
-                    </div>
-                </div>
+                <ProjectCard project={project} />
             ))}
         </div>
-        // <ul className="row">
-        //     {projects.map((project) => (
-        //         <li key={project.id}>{project.name}</li>
-        //     ))}
-        // </ul>
     );
 }
 
