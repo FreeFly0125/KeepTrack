2ab9eb83c8bd67d3eecc73e826b53c023bf23a53
asfreeasfly <simonbergman516@gmail.com>
2023-09-24 19:16:50 +0200

    implement Lab 7: Displaying List Data

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -6,7 +6,29 @@ interface ProjectListProps {
 }
 
 function ProjectList({ projects }: ProjectListProps) {
-    return <pre>{JSON.stringify(projects, null, ' ')}</pre>
+    return (
+        <div className="row">
+            {projects.map((project) => (
+                <div key={project.id} className="cols-sm">
+                    <div className="card">
+                        <img src={project.imageUrl} alt={project.name}></img>
+                        <article className="section dark">
+                            <h5 className="strong">
+                                {project.name}
+                            </h5>
+                            <p>{project.description}</p>
+                            <p>Budget: {project.budget.toString()}</p>
+                        </article>
+                    </div>
+                </div>
+            ))}
+        </div>
+        // <ul className="row">
+        //     {projects.map((project) => (
+        //         <li key={project.id}>{project.name}</li>
+        //     ))}
+        // </ul>
+    );
 }
 
 export default ProjectList;
