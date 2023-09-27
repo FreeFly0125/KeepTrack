769e711a04fca626eb3c5c946803d42ef689aa2e
asfreeasfly <simonbergman516@gmail.com>
2023-09-27 04:44:16 +0200

    implement Lab 15: Form Values to State

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,16 +1,25 @@
+import { useState } from 'react';
 import ProjectList from './ProjectList';
 import { MOCK_PROJECTS } from './MockProjects';
 import { Project } from './Project';
 
 function ProjectsPage() {
+    const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+
     const saveProject = (project: Project) => {
-        console.log("Saving project: ", project);
+        let updatedProjects = projects.map((p: Project) => {
+            return p.id === project.id ? project : p;
+        });
+        setProjects(updatedProjects);
     }
 
     return (
         <>
             <h1>Projects</h1>
-            <ProjectList projects={MOCK_PROJECTS} onSave={saveProject} />
+            <ProjectList
+                projects={projects}
+                onSave={saveProject}
+            />
         </>
     );
 }
